5fc258d600b58f9d7d1e91edd7871338fef3d2f8
ZioXerXes <88509035+ZioXerXes@users.noreply.github.com>
2022-04-20 23:47:44 -0400

    Assignment completed; icing on cake from here

--- js/application.js
+++ js/application.js
@@ -4,7 +4,7 @@ var sum = function (acc, x) {
 
 var updateItemTotal = function (ele) {
   var priceNumber = parseFloat($(ele).children('.price').text());
-  var quantityNumber = parseFloat($(ele).children('.quantity').text());
+  var quantityNumber = parseFloat($(ele).find('.quantity input').val());
   var totalPrice = priceNumber * quantityNumber;
   $(ele).children('.total').html(totalPrice);
   
@@ -23,11 +23,38 @@ var addEverything = function () {
     $('.completeTotal').html(addEmUp);
 };
 
+$(document).on('click', '.btn.remove', function (event) {
+  $(this).closest('tr').remove();
+  addEverything();
+});
+
 $(document).ready(function () {
   addEverything();
+
+  var timeout;
+  $(document).on('input', 'tr input', function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      addEverything();
+    }, 250);
+  });
 
   $('.btn.remove').on('click', function (event) {
     $(this).closest('tr').remove();
     addEverything();
   });
-});
+
+  $('#addGrocery').on('submit', function (event) {
+    event.preventDefault();
+    var name = $(this).children('[name=item]').val();
+    var price = $(this).children('[name=price]').val();
+
+    $('tbody').append('<tr>' +
+    '<td class="name col-xs-3">' + name + '</td>' +
+    '<td class="price col-xs-3">' + price + '</td>' +
+    '<td class="quantity col-xs-3"><input type="number" value="0" /></td>' +
+    '<td class="total">0</td>' +
+    '<td><button class="btn btn-light btn-sm remove">Remove</button></td>' +
+    '</tr>');
+  });
+  });
